762bda04158112b7f448d94c5d80b60d2463a13f
Giovanna Moeller <giovannagmoeller@gmail.com>
2022-09-21 16:17:45 -0300

    categories

---
+++ src/lib/category.js
@@ -0,0 +1,63 @@
+export const categories = [
+    {
+        name: 'Redes Sociais',
+        slug: 'social-media',
+        links: [
+            {
+                name: 'Instagram',
+                url: 'https://instagram.com/girl.coding'
+            },
+            {
+                name: 'Youtube',
+                url: 'https://youtube.com/c/GirlCoding'
+            },
+            {
+                name: 'Medium',
+                url: 'https://medium.com/@giovannamoeller'
+            },
+            {
+                name: 'Github',
+                url: 'https://github.com/giovannamoeller'
+            },
+            {
+                name: 'Linkedin',
+                url: 'https://linkedin.com/in/giovannamoeller'
+            },
+            {
+                name: 'Entre em Contato',
+                url: 'mailto:contact@giovannamoeller.com'
+            },
+        ]
+    }, 
+    {
+        name: 'Artigos',
+        slug: 'articles'
+    },
+    {
+        name: '7 Days of Code',
+        slug: '7days-of-code',
+        description: 'Pratique seus conhecimentos com esse desafio gratuito que eu realizei em parceria com a Alura. Sete dias de desafios diários. Na tecnologia que você está aprendendo. Pratique, estude e mostre seu portfolio.',
+        links: [
+            {
+                name: 'HTML e CSS - Landing Page',
+                url: 'https://7daysofcode.io/matricula/html-css'
+            },
+            {
+                name: 'Consumindo uma API com Javascript Puro',
+                url: 'https://7daysofcode.io/matricula/javascript-e-dom-api'
+            },
+        ]
+    },
+    {
+        name: 'Meu Setup',
+        slug: 'setup'
+    },
+    {
+        name: 'Livros de Programação',
+        slug: 'books'
+    },
+    {
+        name: 'Eventos de Tecnologia',
+        slug: 'events'
+    },
+]
